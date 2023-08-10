3761125a07f3d33bab02760679e78075c44f98e5
Chris1998c <contechristian.98@gmail.com>
2023-08-10 16:35:41 +0200

    file

---
+++ exercise.js
@@ -0,0 +1,72 @@
+function uncompletedNotes(notes) {
+  const uncompletedTodos = [];
+
+  notes.forEach(note => {
+    const uncompletedNoteTodos = note.todos.filter(todo => !todo.done);
+    if (uncompletedNoteTodos.length > 0) {
+      uncompletedTodos.push({
+        id: note.id,
+        description: note.description,
+        nome: note.name,
+        todos: uncompletedNoteTodos
+      });
+    }
+  });
+
+  return uncompletedTodos;
+}
+
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    name: "Push ups - 10 x 3",
+    todos: [
+      {
+        id: 1,
+        name: "Push ups - 10 x 3",
+        done: false,
+      },
+      {
+        id: 2,
+        name: "Abdominals - 20 x 3",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      {
+        id: 1,
+        name: "Learn HTML",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Learn CSS",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Learn JavaScript",
+        done: true,
+      },
+      {
+        id: 4,
+        name: "Learn Angular",
+        done: false,
+      },
+    ],
+  },
+];
+
+
+const uncompleted = uncompletedNotes(notes);
+console.log(uncompleted);
